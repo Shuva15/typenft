7376e5c79eb48346c30e949f1c71aec38bae6714
onlymepujja <152049633+onlymepujja@users.noreply.github.com>
2023-11-29 22:32:21 +0000

    Added Styles and restructured the project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import MintNft from './components/MintNft'
-import { authenticate, unauthenticate } from '@onflow/fcl'
-import useCurrentUser from './lib/utils';
+
 
 function App() {
-  const { addr, loggedIn } = useCurrentUser();
-  console.log(addr)
   return (
     <div className="App">
-      {loggedIn ? <button onClick={unauthenticate}>log out</button> : <button onClick={authenticate}>connect</button>}
-      {loggedIn && <MintNft addr={addr} />}
+      <MintNft />
     </div>
   );
 }
