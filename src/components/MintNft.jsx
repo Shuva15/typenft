7bafe789d9192a58cea9563f9270a4871e989c0a
Shuva15 <shuvajitmahanta02@gmail.com>
2023-11-29 19:37:51 +0530

    implemented the ai image generation

--- src/components/MintNft.jsx
+++ src/components/MintNft.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MintNft = () => {
+    const [nft, setNft] = useState(null)
+
+    async function query(data) {
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/nerijs/pixel-art-xl",
+            {
+                headers: { Authorization: "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" },
+                method: "POST",
+                body: JSON.stringify(data),
+            }
+        ); 
+        const result = await response.blob();
+        return result;
+    }
+
+    const handleClick = () => {
+        query({"inputs": "Astronaut riding a horse"}).then((response) => {
+            setNft(URL.createObjectURL(response))
+        });
+    }
+
   return (
     <div>
       <input type="text" name="" id="" />
-      <button></button>
+      <button onClick={handleClick}>mint</button>
+      {nft && <img src={nft}></img>}
     </div>
   );
 };
